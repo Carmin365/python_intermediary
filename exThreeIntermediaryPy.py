fe9dd3f344a1b82e843bdf06e4d888ccd8c5f14a
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-07-17 19:43:28 -0300

    Update exThreeIntermediaryPy.py

--- exThreeIntermediaryPy.py
+++ exThreeIntermediaryPy.py
@@ -4,7 +4,6 @@
 # Line 4 >> This line characterizes the beginning of the for loop and uses the variable args. # Esta linha caracteriza o inicio do laço for e utiliza a vaiável args.
 # Line 5 >> This line features an 'if' conditional structure that uses the isinstance() function to confirm that the 'arg' object is not an instance of the type described in 'data_type'. # Esta linha caracteriza uma estrutura condicional 'if' que usa a função isinstance() para confirmar se o objeto 'arg' não é uma instância do tipo descrito em 'tipo_dado'. 
 # Line 6 >> This line indicates an exception from the 'TypeError' pattern with an edited warning, showing that the argument provided is not of the expected pattern. # Esta linha indica uma exceção do padrão 'TypeError' com um aviso editado, mostrando que o argumento disponibilizado não é do padrão esperado. 
-
 def autenticar_acesso_decorador(tipo_dado):
     def decorator(funcao):
         def funcao_decorada(*args, **kwargs):
